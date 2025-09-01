bab4530813ee9673886e5e7f3e69fa9ab565efc7
unknown <Administrator@ITSYS13.medicity.co.in>
2025-09-01 13:07:17 +0530

    punch transfer null array correction

--- api/PunchTransfer/punchtransfer.controller.js
+++ api/PunchTransfer/punchtransfer.controller.js
@@ -7,6 +7,7 @@ module.exports = {
         const body = req.body;
         getpunchdataZtech(body, (err, results) => {
             const zkteco = [...results]
+
             if (err) {
                 logger.errorLogger(err)
                 return res.status(200).json({
@@ -28,23 +29,30 @@ module.exports = {
                         return values.id === value.id
                     })
                 })
-                var a1 = newpunchdata.map((value, index) => {
-                    return [value.id, value.emp_code, value.punch_time, value.punch_state
-                    ]
-                })
-                InsertPunchdata(a1, (err, results) => {
-                    if (err) {
-                        logger.logwindow(err)
-                        return res.status(200).json({
-                            success: 0,
-                            message: err
-                        });
-                    }
+                if (newpunchdata.length === 0) {
                     return res.status(200).json({
                         success: 1,
                         message: "Punch Data Transfer Successfully"
                     });
-                });
+                } else {
+                    var a1 = newpunchdata.map((value, index) => {
+                        return [value.id, value.emp_code, value.punch_time, value.punch_state
+                        ]
+                    })
+                    InsertPunchdata(a1, (err, results) => {
+                        if (err) {
+                            logger.logwindow(err)
+                            return res.status(200).json({
+                                success: 0,
+                                message: err
+                            });
+                        }
+                        return res.status(200).json({
+                            success: 1,
+                            message: "Punch Data Transfer Successfully"
+                        });
+                    });
+                }
             });
         });
     },
